a4996e94fcf67d34af1b4e5f133d16106351988e
liupei8979 <khsr98@ajou.ac.kr>
2023-06-17 20:17:45 +0900

    #5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,8 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
-}
-
-setInterval(sayHello, 5000);
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  }
+  getClock();
+  setInterval(getClock, 1000);
