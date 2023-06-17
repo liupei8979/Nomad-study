50ec8a1f440465a5466d10d201bdd42b2009f561
liupei8979 <khsr98@ajou.ac.kr>
2023-06-17 18:29:26 +0900

    4.1 Form Submission

---
+++ app.js
@@ -0,0 +1,9 @@
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  console.log("hello", loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
